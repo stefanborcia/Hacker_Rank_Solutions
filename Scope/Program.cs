d209b3d6e1c523bc121009c9b20f2a624455f204
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-12-19 18:53:41 +0100

    how to check the maximum absolute difference between 2 integers in an array

---
+++ Scope/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] arrDif)
+    {
+        this.elements = arrDif;
+    }
+    public int computeDifference()
+    {
+        Array.Sort(elements);
+        int minValue = elements[0];
+        int maxValue = elements[elements.Length - 1];
+        maximumDifference = maxValue - minValue;
+        return maximumDifference;
+    }
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
