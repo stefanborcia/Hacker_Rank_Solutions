49cf49246720df4f99080b3c6e8a480964eefa08
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-12-28 15:53:00 +0100

    Sorting algorithm

---
+++ Sorting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        // Write your code here
+
+        int numberOfSwaps = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (a.ElementAt(j) > a.ElementAt(j + 1))
+                {
+                    //Swapping elements
+                    int tempVal = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tempVal;
+
+                    //Increasing swap count
+                    numberOfSwaps++;
+                }
+            }
+        }
+        Console.WriteLine("Array is sorted in {0} swaps.", numberOfSwaps);
+        Console.WriteLine("First Element: {0}", a.ElementAt(0));
+        Console.WriteLine("Last Element: {0}", a.ElementAt(n - 1));
+    }
+}
