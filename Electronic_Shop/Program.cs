1fa3c7705ef51934bcb170fdb71aae98286a16ba
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-01-15 17:50:22 +0100

    HackerRank Solution Electronic Shop using Linq and Lambda

---
+++ Electronic_Shop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+class Solution
+{
+
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b)
+    {
+        /*
+         * Write your code here.
+         */
+        List<int> allPrice = keyboards.SelectMany(k => drives.Select(d => d + k))
+            .Where(i => i <= b)
+            .OrderByDescending(i => i)
+            .ToList();
+
+        if (allPrice.Count == 0)
+        {
+            return -1;
+        }
+
+        return allPrice.First();
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] bnm = Console.ReadLine().Split(' ');
+
+        int b = Convert.ToInt32(bnm[0]);
+
+        int n = Convert.ToInt32(bnm[1]);
+
+        int m = Convert.ToInt32(bnm[2]);
+
+        int[] keyboards = Array.ConvertAll(Console.ReadLine().Split(' '), keyboardsTemp => Convert.ToInt32(keyboardsTemp))
+            ;
+
+        int[] drives = Array.ConvertAll(Console.ReadLine().Split(' '), drivesTemp => Convert.ToInt32(drivesTemp))
+            ;
+        /*
+         * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+         */
+
+        int moneySpent = getMoneySpent(keyboards, drives, b);
+
+        textWriter.WriteLine(moneySpent);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
